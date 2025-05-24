e088970f0a7d2ba29dfc0be29025370cd3a81526
codigoinerte <fmartinez.bpe@gmail.com>
2025-05-24 16:04:38 -0500

    fix: cambio de nombre a archivo

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ require_once __DIR__. '/vendor/autoload.php';
 $path = "https://inmobaperu.com";
 $path_data_website = __DIR__ . "/scraping_" . date("Ymd") . ".log";
 
-require_once __DIR__  . '/inmobaperu.php';
+require_once __DIR__  . '/Inmobaperu.php';
